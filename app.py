1db48dea0322fac1f33325c17fb0c0f0c097e618
camithanna-web <camithanna@gmail.com>
2026-03-17 10:06:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,15 @@
+app.py
 from flask import Flask, jsonify, request
-
 app = Flask(__name__)
 
-# Home route
 @app.route('/')
 def home():
-    return jsonify({
-        "message": "Welcome to my Flask API!",
-        "status": "API is running"
-    })
+return "Welcome to my Flask API!"
 
-# Student information
-@app.route('/student', methods=['GET'])
+@app.route('/student')
 def get_student():
-    student = {
-        "name": "Your Name",
-        "grade": 10,
-        "section": "Zechariah"
-    }
-    return jsonify(student)
-
-# Add student using POST
-@app.route('/add_student', methods=['POST'])
-def add_student():
-    data = request.get_json()
-
-    new_student = {
-        "name": data.get("name"),
-        "grade": data.get("grade"),
-        "section": data.get("section")
-    }
-
-    return jsonify({
-        "message": "Student added successfully",
-        "student": new_student
-    })
-
-if __name__ == "__main__":
-    app.run(debug=True)
+return jsonify({
+"name": "Your Name",
+"grade": 10,
+"section": "Zechariah"
+})
